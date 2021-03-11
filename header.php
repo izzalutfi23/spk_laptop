d37a094b0f145116c5c661c33353a58fee11f739
izza lutfi <izzalutfi045@gmail.com>
2021-03-12 01:10:00 +0700

    update title

--- header.php
+++ header.php
@@ -26,7 +26,7 @@ The above copyright notice and this permission notice shall be included in all c
     <link rel="apple-touch-icon" sizes="76x76" href="assets/img/apple-icon.png">
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
     <title>
-        Kicau Mania
+        Nadella | SPK Pemilihan Laptop
     </title>
     <meta content='width=device-width, initial-scale=1.0, shrink-to-fit=no' name='viewport' />
     <!--     Fonts and icons     -->
